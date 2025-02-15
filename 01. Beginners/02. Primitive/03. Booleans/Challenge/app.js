aaf77ef2562f0c7cf535703d3f49761a0146c9c0
safwan-ms <safwan.anasms@gmail.com>
2025-02-15 22:00:05 +0530

    Learned Comparison Operator in JS

--- 01. Beginners/02. Primitive/03. Booleans/Challenge/app.js
+++ 01. Beginners/02. Primitive/03. Booleans/Challenge/app.js
@@ -1,11 +1,11 @@
-let isLoggedIn = NaN;
-let numbers = 10;
-console.log(numbers + undefined);
+// 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
+// 2. Create variable name (isJsHard) & store false as a value.
+// 3. Create variable name (favNumb) & store your favorite number inside.
+// 4. Now Add favNumber with the value of undefined.
 
-// false
-// null
-// undefined
-// 0
-// -0
-// NaN
-// '', "", ``, (empty strings)
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 21;
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb + undefined);
