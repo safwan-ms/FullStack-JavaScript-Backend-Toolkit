458ad18ec3f82442a1cba6a7eb88049ea751add9
safwan-ms <safwan.anasms@gmail.com>
2025-04-29 19:14:37 +0530

    Template String and Arrow Function

---
+++ 01. JavaScript/02. Intermediate/01. Template String/live coding/app.js
@@ -0,0 +1,10 @@
+let firstName = "Safwax";
+let lastName = "WebDev";
+
+function intro() {
+  return "Hello my name is Safwax WebDev & i'm 19 years old";
+}
+
+console.log(
+  `Hello my name is ${firstName} ${lastName} & i'm ${19 + 1000} years old.`
+);
