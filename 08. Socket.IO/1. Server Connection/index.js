a5553c7739eeb9039c0ca2b01f0c6f72c55c62e4
safwan-ms <safwan.anasms@gmail.com>
2025-05-20 13:29:55 +0530

    Server Connection

---
+++ 08. Socket.IO/1. Server Connection/index.js
@@ -0,0 +1,21 @@
+//1. packages
+import express from "express";
+import http from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { Server } from "socket.io";
+
+//2. Instances
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//3. Serving HTML File
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+//4. Define a connection event handler
+
+//5. Start the server
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
