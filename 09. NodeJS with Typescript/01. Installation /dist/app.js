4927bfdf890a3235781c69e9d1aab56a34947790
safwan-ms <safwan.anasms@gmail.com>
2025-08-29 19:49:22 +0530

    Setup for NodeJS with Typescript

--- 09. NodeJS with Typescript/01. Installation /dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello NodeJs from typescript");
-function getName(name) {
-    return name;
-}
-const result = getName("Safwax");
-console.log(result);
-//# sourceMappingURL=app.js.map
--- 09. NodeJS with Typescript/01. Installation /src/app.ts
+++
@@ -1,8 +0,0 @@
-console.log("Hello NodeJs from typescript");
-
-function getName(name: string) {
-  return name;
-}
-const result = getName("Safwax");
-
-console.log(result);
