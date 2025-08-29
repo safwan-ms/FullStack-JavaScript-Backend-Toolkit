1a87030cdadc789e068bc141d3f7bf72db908395
safwan-ms <safwan.anasms@gmail.com>
2025-08-29 23:10:28 +0530

    Express Imports

---
+++ 09. NodeJS With Typescript/02. Express Basics/02. Express Imports/app.ts
@@ -0,0 +1,7 @@
+import express, { Express, Response, Request, NextFunction } from "express";
+
+// Express - Main framework
+// Express - Type for the app instance
+// Response - Type for response object
+// Request - Type for request object  
+// NextFunction - Type for next middleware function
